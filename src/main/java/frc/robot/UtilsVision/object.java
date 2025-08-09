61ffda9720ad3d057a5a3bb9d781618006b62de6
dvir_levy <69078741+jarman155@users.noreply.github.com>
2025-08-09 19:05:42 +0300

    added a constractor for the subsystem of object

--- src/main/java/frc/robot/UtilsVision/object.java
+++ src/main/java/frc/robot/UtilsVision/object.java
@@ -4,18 +4,47 @@
 
 package frc.robot.UtilsVision;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.UtilsVision.Cemera;
 
 public class object extends SubsystemBase {
-  /** Creates a new object. */
+  private Translation2d robotToTag;
+  private Translation2d cameraToTag;
+
+  private NetworkTable Table;
+  private double wantedPip = 0;
+
+
+  private double camToObjectYaw;
+  private double camToObjectPitch;
+
+  
+  private Supplier<Rotation2d> getRobotAngle;
+  private Supplier<Pose2d> currentPose;
+
+
   private Cemera camera;
-  public object(Cemera camera) {
+  public object(Cemera camera, Supplier<Rotation2d> getRobotAngle,Supplier<Pose2d> currentPose) {
+    this.getRobotAngle = getRobotAngle;
+    this.currentPose = currentPose;
+
     this.camera = camera;
+    Table = NetworkTableInstance.getDefault().getTable(camera.getTableName());
   }
 
   @Override
   public void periodic() {
+    if(Table.getEntry("tv").getDouble(0.0) != 0){
+      
+    }
     // This method will be called once per scheduler run
   }
 }
