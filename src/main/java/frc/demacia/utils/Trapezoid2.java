12fe1c8664f90646d2b8311b58ca6946e5f78acd
KislevUdi <udi.kislev@gmail.com>
2025-09-18 17:45:22 +0300

    Enhance Trapezoid2 class with additional velocity calculation methods for target position and angle

--- src/main/java/frc/demacia/utils/Trapezoid2.java
+++ src/main/java/frc/demacia/utils/Trapezoid2.java
@@ -1,12 +1,29 @@
 package frc.Demacia.utils;
 
+import edu.wpi.first.math.MathUtil;
 
 public class Trapezoid2 {
+    /**
+     * Calculate the trapezoid velocity to reach the target position
+     * @param remainingDistance
+     * @param curentVelocity
+     * @param targetVelocity
+     * @param maxVelocity
+     * @param maxAcceleration
+     * @return next cycle velocity
+     */
     public static double calculate(double remainingDistance, double curentVelocity, double targetVelocity, double maxVelocity, double maxAcceleration) {
         if(remainingDistance > 0 && targetVelocity >= 0) {
             // calculate the maximum velocity if continuing acceleration/deacceleratin regardless of maxVelocity
-            double maxV = Math.sqrt(remainingDistance*maxAcceleration + curentVelocity*curentVelocity + targetVelocity * targetVelocity);
-            // define the max velocity between the calculated on and the provided one
+            // time to reach maxV = (maxV - currentVelocity) / maxA
+            // distance to maxV = average velocity * time = (maxV + currentVelocity)/2 * (maxV - currentVelocity) / maxAcceleration = 
+            //         (maxV^2 - currentVelocity^2)/(2 * maxAcceleration)
+            // same for distance to target = (maxV^2 - targetVelocity^2)/(2 * maxAcceleration)
+            // remainingDistance = (maxV^2 - targetVelocity^2)/(2 * maxAcceleration) + (maxV^2 - currentVelocity^2)/(2 * maxAcceleration)
+            // remainingDistance * 2 * maxAcceleration = 2*maxV^2 - targetVelocity^2 - currentVelocity^2
+            // maxV^2 = remainingDistance * maxAcceleration + (targetVelocity^2 + currentVelocity^2)/2
+            double maxV = Math.sqrt(remainingDistance*maxAcceleration + (curentVelocity*curentVelocity + targetVelocity * targetVelocity)/2);
+            // define the max velocity between the calculated and the provided
             maxV = Math.min(maxV, maxVelocity);
             // calculate the deacceleation time from maxV to targetVelocity
             double deaccelrateTime = (maxV - targetVelocity) / maxAcceleration;
@@ -26,6 +43,33 @@ public class Trapezoid2 {
         } else { // we need to move forward, target is behind - return the target velocity 
             return targetVelocity;
         }
+    }
+
+    /**
+     * calculate the trapezoid velocity - with target velocity 0
+     * @param remainingDistance
+     * @param curentVelocity
+     * @param maxVelocity
+     * @param maxAcceleration
+     * @return
+     */
+    public static double calculate(double remainingDistance, double curentVelocity, double maxVelocity, double maxAcceleration) {
+        return calculate(remainingDistance, curentVelocity, 0, maxVelocity, maxAcceleration);
+    }
+
+    /**
+     * calculate the trapezoid velocity to reach the target angle from current angle/position in degrees
+     * @param currentAngle - degrees
+     * @param targetAngle - degrees
+     * @param curentVelocity - degrees per sec
+     * @param targetVelocity 
+     * @param maxVelocity
+     * @param maxAcceleration
+     * @return
+     */
+    public static double calculate(double currentAngle, double targetAngle, double curentVelocity, double targetVelocity, double maxVelocity, double maxAcceleration) {
+        double distance = MathUtil.inputModulus(targetAngle - currentAngle,-180, 180);
+        return calculate(distance, curentVelocity, targetVelocity, maxVelocity, maxAcceleration);
     }
     public static void main(String[] args) {
         double startTime = System.currentTimeMillis();
