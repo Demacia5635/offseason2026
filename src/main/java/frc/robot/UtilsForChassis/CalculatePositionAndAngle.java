07efa01f8170077055f1896eed42db88d79e8e9d
Orr Ben Yakar <orbenyak@gmail.com>
2025-09-22 14:50:14 +0300

    Vision-CalculatePositionAndAngleEND

--- src/main/java/frc/robot/UtilsForChassis/CalculatePositionAndAngle.java
+++ src/main/java/frc/robot/UtilsForChassis/CalculatePositionAndAngle.java
@@ -7,6 +7,11 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import frc.robot.UtilsVision.Camera;
+
 import java.util.function.Supplier;
 /** Add your docs here. */
 public class CalculatePositionAndAngle {
@@ -14,12 +19,17 @@ public class CalculatePositionAndAngle {
     private Supplier<ChassisSpeeds> speeds;
     private Supplier<Rotation2d> getRobotAngle;
     private double dtseconds = 0.02;
+    private Camera camera;
+     private NetworkTable Table;
 
-    public static Translation2d computeDeltaPosition(Supplier<ChassisSpeeds> speeds, Supplier<Rotation2d> getRobotAngle, double dtseconds) {
+    public Translation2d computeDeltaPosition(Camera camera,Supplier<ChassisSpeeds> speeds, Supplier<Rotation2d> getRobotAngle, double dtseconds) {
+        this.camera = camera;
+        Table = NetworkTableInstance.getDefault().getTable(camera.getTableName());
+        latency = Table.getEntry("tl").getDouble(0.0) + Table.getEntry("cl").getDouble(0.0);
 
         Translation2d relativeMovement = new Translation2d(
-                speeds.get().vxMetersPerSecond * dtseconds,
-                speeds.get().vyMetersPerSecond * dtseconds
+                speeds.get().vxMetersPerSecond * dtseconds+latency,
+                speeds.get().vyMetersPerSecond * dtseconds+latency
         );
         
         return relativeMovement.rotateBy(getRobotAngle.get());
