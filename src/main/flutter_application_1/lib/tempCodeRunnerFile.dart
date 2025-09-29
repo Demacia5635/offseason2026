f3f0d22c08be41d52dca04365cbddb8274743c25
matan <diz.matan@gmail.com>
2025-09-29 15:30:12 +0300

    i fix the error in the class tempCodeRunnerFile

--- src/main/flutter_application_1/lib/tempCodeRunnerFile.dart
+++ src/main/flutter_application_1/lib/tempCodeRunnerFile.dart
@@ -1,4 +1,13 @@
- return MaterialApp(
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("FRC Field with Background")),
         body: const FRCField(), // Remove Center widget
@@ -11,5 +20,15 @@ class FRCField extends StatefulWidget {
   const FRCField({super.key});
 
   @override
-  _FRCFieldState createState() => _FRCFieldState();
+  FRCFieldState createState() => FRCFieldState(); // Removed underscore to make it public
+}
+
+class FRCFieldState extends State<FRCField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Add your FRC field implementation here
+      child: const Text("FRC Field Content"),
+    );
+  }
 }
