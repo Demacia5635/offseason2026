38c8feb7934fc1543b5a634542d18e910cc0a8b4
Orr Ben Yakar <orbenyak@gmail.com>
2025-09-29 17:38:46 +0300

    vision-fixed the measure units

--- src/main/java/frc/robot/UtilsForChassis/CalculatePositionAndAngle.java
+++ src/main/java/frc/robot/UtilsForChassis/CalculatePositionAndAngle.java
@@ -4,14 +4,16 @@
 
 package frc.robot.UtilsForChassis;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.UtilsVision.Camera;
-
+import edu.wpi.first.wpilibj.DriverStation;
 import java.util.function.Supplier;
 /** Add your docs here. */
 public class CalculatePositionAndAngle {
@@ -20,18 +22,20 @@ public class CalculatePositionAndAngle {
     private Supplier<Rotation2d> getRobotAngle;
     private double dtseconds = 0.02;
     private Camera camera;
-     private NetworkTable Table;
+    public Pose2d pose;
+    private NetworkTable Table;
+    public boolean is3D;
 
     public Translation2d computeDeltaPosition(Camera camera,Supplier<ChassisSpeeds> speeds, Supplier<Rotation2d> getRobotAngle, double dtseconds) {
-        this.camera = camera;
-        Table = NetworkTableInstance.getDefault().getTable(camera.getTableName());
-        latency = Table.getEntry("tl").getDouble(0.0) + Table.getEntry("cl").getDouble(0.0);
-
-        Translation2d relativeMovement = new Translation2d(
-                speeds.get().vxMetersPerSecond * dtseconds+latency,
-                speeds.get().vyMetersPerSecond * dtseconds+latency
-        );
-        
+      
+          latency = Table.getEntry("tl").getDouble(0.0) + Table.getEntry("cl").getDouble(0.0)*(10^3);
+    
+    Translation2d relativeMovement = new Translation2d(
+            speeds.get().vxMetersPerSecond * dtseconds+latency,
+            speeds.get().vyMetersPerSecond * dtseconds+latency
+     );
         return relativeMovement.rotateBy(getRobotAngle.get());
+        
     }
 }
+
