3b3f5348773fe0941fde39a2dc8acf211f88328d
matan <diz.matan@gmail.com>
2025-10-04 23:28:39 +0300

    i cange remrk from ebrow to englise

--- src/main/flutter_application_1/lib/utils/utils.dart
+++ src/main/flutter_application_1/lib/utils/utils.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const double metersToPixelsScale = 20.0; // 1 מטר = 20 פיקסלים
+  static const double metersToPixelsScale = 20.0; // 1 meter 20 pexels
   static const double robotWidthMeters = 0.8;
   static const double robotHeightMeters = 0.7;
   static const String fieldImagePath = 'assets/frcField2025.jpeg';
