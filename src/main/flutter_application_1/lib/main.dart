e7c6583c88910f8e8280957eae50727bbc575035
matan <diz.matan@gmail.com>
2025-10-05 13:06:40 +0300

    i finas main

--- src/main/flutter_application_1/lib/main.dart
+++ src/main/flutter_application_1/lib/main.dart
@@ -3,12 +3,28 @@ import 'package:flutter_application_1/back/MouseState.dart';
 import 'package:flutter_application_1/front/fieldImage.dart';
 import 'package:flutter_application_1/front/mouseCoordinatesDisplay.dart';
 import 'package:flutter_application_1/front/painter.dart';
+import 'package:flutter_application_1/utils/utils.dart';
 
-//delete
 void main() {
+
+  Container(
+    child: Row(
+      children: [
+        MouseState(),
+        FieldImage(),
+        MouseCoordinatesDisplay(xMeters:10 ,yMeters:5 ),
+        Painter(height:Constants.robotHeightPixels ,width:Constants.robotWidthPixels  ,firstPoint:null ,secondPoint: null)
+      ],
+    )
+  );
+
+  runApp(Container());
+
+  /*
     runApp(MaterialApp());
     runApp(MouseCoordinatesDisplay(xMeters: 0, yMeters: 0));
     runApp(Painter(height: 200, width: 200, firstPoint: null, secondPoint: null));
     runApp(FieldImage());
     runApp(MouseState());
+    */
 }
