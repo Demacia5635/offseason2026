51c27847a018ec659bab4b5c79373e0ca8adff6b
matan <diz.matan@gmail.com>
2025-10-05 12:10:40 +0300

    i cange that the pixel to meter well be as the scrren saze

--- src/main/flutter_application_1/lib/utils/utils.dart
+++ src/main/flutter_application_1/lib/utils/utils.dart
@@ -1,6 +1,8 @@
 class Constants {
-  static const double metersToPixelsScale = 20.0; // 1 meter 20 pexels
   static const double robotWidthMeters = 0.8;
   static const double robotHeightMeters = 0.7;
   static const String fieldImagePath = 'assets/frcField2025.jpeg';
+  static const double frcFieldSize = 7.9;
+  static const double PixelsToMeters= double.infinity/frcFieldSize;
+  static const double metersToPixelsScale = 1000/PixelsToMeters;
 }
