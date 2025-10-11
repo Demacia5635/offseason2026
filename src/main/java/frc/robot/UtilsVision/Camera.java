e3c4e001ca0a30bf4ad667e169eca13823ca98c4
nanonoam <97607867+nanonoam@users.noreply.github.com>
2025-10-11 09:19:38 +0300

    add past code

--- src/main/java/frc/robot/UtilsVision/Camera.java
+++ src/main/java/frc/robot/UtilsVision/Camera.java
@@ -1,7 +1,6 @@
 package frc.robot.UtilsVision;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 
 public class Camera {
     private String name;
