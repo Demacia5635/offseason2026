995d497c5637c7acaf6d891ad6119fd38038ffb3
matan <diz.matan@gmail.com>
2025-10-22 21:18:22 +0300

    added a sumluation mode not working (and i dont think we need it just adding it if will use it)

--- src/main/java/frc/robot/vision/Quest.java
+++ src/main/java/frc/robot/vision/Quest.java
@@ -1,28 +1,29 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import gg.questnav.questnav.PoseFrame;
 import gg.questnav.questnav.QuestNav;
 
 public class Quest extends SubsystemBase {
-  private double PoseFrame;
   private Pose2d questPose;
-  QuestNav questNav = new QuestNav();
+  QuestNav questNav;
+  
   public Quest() {
+    // For simulation, connect to localhost
+    if (RobotBase.isSimulation()) {
+      NetworkTableInstance.getDefault().setServer("localhost");
+    }
+    
+    questNav = new QuestNav();
 
     // Get the latest pose data frames from the Quest
     PoseFrame[] poseFrames = questNav.getAllUnreadPoseFrames();
 
     if (poseFrames.length > 0) {
-    // Get the most recent Quest pose
       questPose = poseFrames[poseFrames.length - 1].questPose();
-
     }
   } 
 
@@ -32,8 +33,6 @@ public class Quest extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
     questNav.commandPeriodic();
   }
-
 }
