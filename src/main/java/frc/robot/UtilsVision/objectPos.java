603daa39219f69a1b053e353f61f3c71f4ac3edc
Orr Ben Yakar <orbenyak@gmail.com>
2025-10-26 17:23:17 +0200

    version that need to chack

--- src/main/java/frc/robot/UtilsVision/objectPos.java
+++ src/main/java/frc/robot/UtilsVision/objectPos.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.UtilsVision;
+import frc.robot.vision.Tag;
 import frc.robot.vision.utils.Camera;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -13,6 +14,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -21,14 +24,12 @@ public class objectPos extends SubsystemBase {
   private Translation2d robotToObject;
   private Translation2d cameraToObject;
   private Supplier<Rotation2d> getRobotAngle;
-  private double camToObjectYaw;
   private double ty;
-  private double id;
   private double height;
   private double distX;
   private Translation2d originToRobot;
-  private Translation2d origintoObject;
-  public Pose2d pose;
+  private Translation2d originToObject;
+  private Pose2d pose;
   private Supplier<Pose2d> robotCurrentPose;
   private Translation3d robotToCamPosition;
   private double dist;
@@ -36,36 +37,48 @@ public class objectPos extends SubsystemBase {
   
 
   /** Creates a new objectPos. */
-  public objectPos(Camera camera,Supplier<Pose2d> robotCurrentPose,Translation3d robotToCamPosition, Supplier<Rotation2d> getRobotAngle) {
+  public objectPos(Camera camera,Supplier<Pose2d> robotCurrentPose,Translation3d robotToCamPosition, 
+  Supplier<Rotation2d> getRobotAngle, Tag tag) {
       this.robotCurrentPose = robotCurrentPose;
       this.getRobotAngle = getRobotAngle;
+      this.camera = camera;
+      tx = tag.getCameraToTag().getX();
+
+
+      
   }
   
   private double objectDistanceToCamra(){
     height = camera.getRobotToCamPosition().getZ();
     distX = height/Math.tan(Math.toRadians(ty-90));
-    dist =distX/Math.cos(Math.toRadians(ty-90));
+    //dist =distX/Math.cos(Math.toRadians(ty-90));
 
-    return dist;
+    return distX/Math.cos(Math.toRadians(ty-90));//dist;
   }
 
   private Translation2d objectToRobot(){
-    cameraToObject = new Translation2d(objectDistanceToCamra(),new Rotation2d(tx));
-    robotToObject = cameraToObject.plus(camera.getRobotToCamPosition().toTranslation2d());
-    return robotToObject;
+    cameraToObject = new Translation2d(objectDistanceToCamra(), Rotation2d.fromRadians(tx));
+    //robotToObject = cameraToObject.plus(camera.getRobotToCamPosition().toTranslation2d());
+    return cameraToObject.plus(camera.getRobotToCamPosition().toTranslation2d());
 
   }
 
-  private Translation2d objectToOrigin(){
-    robotToObject = objectToRobot().rotateBy(getRobotAngle.get());
-    origintoObject = robotToObject.plus(robotCurrentPose.get().getTranslation());
-
-    return originToRobot;
+  private Translation2d originToObject(){
+    //robotToObject = objectToRobot().rotateBy(getRobotAngle.get());
+    // = robotToObject.plus(robotCurrentPose.get().getTranslation());
+    return robotCurrentPose.get().getTranslation().minus(objectToRobot());
+    // return objectToRobot().rotateBy(getRobotAngle.get()).plus(robotCurrentPose.get().getTranslation());
   }
-
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    originToObject = originToObject();
+
+    
+  }
+
+  public Translation2d getOriginToObject(){
+    assert originToObject == null : "vector is null";
+    return originToObject;
   }
 }
