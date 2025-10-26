24da7b0f32eb94a6f4e6c1f38ae0edef8493d7d9
matan <diz.matan@gmail.com>
2025-10-26 19:15:54 +0200

    now working

--- src/main/java/frc/robot/vision/Quest.java
+++ src/main/java/frc/robot/vision/Quest.java
@@ -1,38 +1,30 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import gg.questnav.questnav.PoseFrame;
 import gg.questnav.questnav.QuestNav;
 
 public class Quest extends SubsystemBase {
   private Pose2d questPose;
-  QuestNav questNav;
+  private QuestNav questNav;
   
   public Quest() {
-    // For simulation, connect to localhost
-    if (RobotBase.isSimulation()) {
-      NetworkTableInstance.getDefault().setServer("localhost");
-    }
-    
     questNav = new QuestNav();
-
-    // Get the latest pose data frames from the Quest
-    PoseFrame[] poseFrames = questNav.getAllUnreadPoseFrames();
-
-    if (poseFrames.length > 0) {
-      questPose = poseFrames[poseFrames.length - 1].questPose();
-    }
+    questPose = new Pose2d();
   } 
 
   public Pose2d getPose() {
-    return this.questPose;
+    return questPose;
   }
 
   @Override
   public void periodic() {
     questNav.commandPeriodic();
+    
+    PoseFrame[] poseFrames = questNav.getAllUnreadPoseFrames();
+    if (poseFrames.length > 0) {
+      questPose = poseFrames[poseFrames.length - 1].questPose();
+    }
   }
 }
