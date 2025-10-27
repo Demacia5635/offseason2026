3c813e845f4a88aead0106739f6d6d0c53058459
matan <diz.matan@gmail.com>
2025-10-27 21:03:56 +0200

    test quest

--- src/main/java/frc/robot/vision/Quest.java
+++ src/main/java/frc/robot/vision/Quest.java
@@ -1,7 +1,5 @@
 package frc.robot.vision;
 
-import java.text.DateFormat.Field;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,29 +8,29 @@ import gg.questnav.questnav.PoseFrame;
 import gg.questnav.questnav.QuestNav;
 
 public class Quest extends SubsystemBase {
-  private Field2d qField2d;
-  private Pose2d questPose;
-  private QuestNav questNav;
-  
+  private final Field2d qField2d = new Field2d();
+  private final QuestNav questNav = new QuestNav();
+
   public Quest() {
-    qField2d = new Field2d();
-    questNav = new QuestNav();
-    questPose = new Pose2d();
     SmartDashboard.putData("quest-map", qField2d);
-  } 
+  }
 
   public Pose2d getPose() {
-    return questPose;
+    return qField2d.getRobotPose();
   }
 
   @Override
   public void periodic() {
-    qField2d.setRobotPose(questPose);
-    questNav.commandPeriodic();
-    
+    questNav.commandPeriodic(); // update QuestNav
+
     PoseFrame[] poseFrames = questNav.getAllUnreadPoseFrames();
     if (poseFrames.length > 0) {
-      questPose = poseFrames[poseFrames.length - 1].questPose();
+      Pose2d latestPose = poseFrames[poseFrames.length - 1].questPose();
+      
+      SmartDashboard.putNumber("questX", latestPose.getX());
+      SmartDashboard.putNumber("questY", latestPose.getY());
+      qField2d.setRobotPose(latestPose); // <-- update field directly
+      SmartDashboard.putData("quest-map", qField2d);
     }
   }
 }
