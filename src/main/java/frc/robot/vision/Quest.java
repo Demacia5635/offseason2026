f67dc530f1b34f06d5d433022a60467bb8f699b9
matan <diz.matan@gmail.com>
2025-10-27 22:36:33 +0200

    now showing on the map but need to fix the quest from reconcting

--- src/main/java/frc/robot/vision/Quest.java
+++ src/main/java/frc/robot/vision/Quest.java
@@ -1,36 +1,51 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import gg.questnav.questnav.PoseFrame;
-import gg.questnav.questnav.QuestNav;
 
 public class Quest extends SubsystemBase {
-  private final Field2d qField2d = new Field2d();
-  private final QuestNav questNav = new QuestNav();
+  private final Field2d field = new Field2d();
+  private final DoubleArraySubscriber positionSub;
+  private final DoubleArraySubscriber eulerAnglesSub;
+  private Pose2d currentPose = new Pose2d();
 
   public Quest() {
-    SmartDashboard.putData("quest-map", qField2d);
+    NetworkTable questTable = NetworkTableInstance.getDefault().getTable("questnav");
+    
+    positionSub = questTable.getDoubleArrayTopic("position").subscribe(new double[]{0, 0, 0});
+    eulerAnglesSub = questTable.getDoubleArrayTopic("eulerAngles").subscribe(new double[]{0, 0, 0});
+    
+    SmartDashboard.putData("Quest Field", field);
   }
 
   public Pose2d getPose() {
-    return qField2d.getRobotPose();
+    return currentPose;
   }
 
   @Override
   public void periodic() {
-    questNav.commandPeriodic(); // update QuestNav
-
-    PoseFrame[] poseFrames = questNav.getAllUnreadPoseFrames();
-    if (poseFrames.length > 0) {
-      Pose2d latestPose = poseFrames[poseFrames.length - 1].questPose();
+    double[] position = positionSub.get();
+    double[] eulerAngles = eulerAnglesSub.get();
+    
+    if (position.length >= 3 && eulerAngles.length >= 3) {
+      double x = position[0];
+      double y = position[1];
+      double rotationDegrees = eulerAngles[2];
       
-      SmartDashboard.putNumber("questX", latestPose.getX());
-      SmartDashboard.putNumber("questY", latestPose.getY());
-      qField2d.setRobotPose(latestPose); // <-- update field directly
-      SmartDashboard.putData("quest-map", qField2d);
+      currentPose = new Pose2d(x, y, Rotation2d.fromDegrees(rotationDegrees));
+      if(currentPose != null){
+        field.setRobotPose(currentPose);
+      }
+      
+      SmartDashboard.putNumber("Quest X", x);
+      SmartDashboard.putNumber("Quest Y", y);
+      SmartDashboard.putNumber("Quest Rotation", rotationDegrees);
     }
   }
 }
