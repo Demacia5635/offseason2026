f049ae1bddebc4239c0fe8f5308d52fa31cf788d
Orr Ben Yakar <orbenyak@gmail.com>
2025-12-04 21:22:24 +0200

    add - edu.wpi.first.GradleRIO version "2025.3.2

--- src/main/java/frc/robot/UtilsVision/object.java
+++ src/main/java/frc/robot/UtilsVision/object.java
@@ -9,13 +9,13 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // import frc.robot.UtilsVision.Camera;
 
+// Subsystem that tracks and calculates the position of a vision target (object) on the field
 public class object extends SubsystemBase {
   private Translation2d robotToObject;
   private Translation2d cameraToObject;
@@ -35,6 +35,10 @@ public class object extends SubsystemBase {
   private Camera camera;
   private Pose2d objectPose;
 
+/**
+   * Constructor - Initializes the object tracker with camera configuration and robot position suppliers.
+   * Sets up the NetworkTable connection to receive vision data from the camera.
+   */
   public object(Camera camera, Supplier<Rotation2d> getRobotAngle,Supplier<Pose2d> robotCurrentPose) {
     this.getRobotAngle = getRobotAngle;
     this.robotCurrentPose = robotCurrentPose;
@@ -43,6 +47,11 @@ public class object extends SubsystemBase {
     Table = NetworkTableInstance.getDefault().getTable(camera.getTableName());
   }
 
+  /**
+   * Periodic method called every robot loop (~20ms).
+   * Reads the latest vision data (pitch, yaw) from NetworkTables and updates the object's field position
+   * if a valid target is detected.
+   */
   @Override
   public void periodic() {
     camToObjectPitch = Table.getEntry("ty").getDouble(0.0);
@@ -53,9 +62,19 @@ public class object extends SubsystemBase {
     
   }
 
+   /**
+   * Returns the last calculated field pose of the tracked object.
+   * @return Pose2d containing the object's position and rotation on the field
+   */
   public Pose2d getPose2d(){
     return objectPose;
   }
+
+  /**
+   * Calculates the straight-line distance from the camera to the detected object.
+   * Uses trigonometry with the camera height, mount angle, and target angle to compute the distance.
+   * @return Distance from camera to object in the same units as camera height
+   */
   public double getDistcameraToObject(){
     double alpha = camToObjectPitch + camera.getPitch();
     Math.toRadians(alpha);
@@ -63,29 +82,51 @@ public class object extends SubsystemBase {
     return distX/( Math.cos(Math.toRadians( (camera.getPitch()+camToObjectPitch) ) ) );
   }
 
+   /**
+   * Calculates the translation vector from the robot's center to the detected object.
+   * First calculates camera-to-object translation, then adds the camera's offset from robot center.
+   * @return Translation2d from robot center to object in robot coordinates
+   */
   public Translation2d getRobotToObject(){
     cameraToObject = new Translation2d(getDistcameraToObject(),camera.getPitch()+camToObjectPitch);
     robotToObject = new Translation2d(camera.getRobotToCamPosition().getX(), camera.getRobotToCamPosition().getY()).plus(cameraToObject);
     return robotToObject;
   }
 
-
+  /**
+   * Calculates the translation vector from the field origin to the detected object.
+   * Rotates the robot-to-object vector by the robot's field angle, then adds the robot's field position.
+   * @return Translation2d from field origin to object in field coordinates
+   */
   public Translation2d getOriginToObject(){
     robotToObject = getRobotToObject().rotateBy(getRobotAngle.get());
     OriginToObject = robotToObject.plus(robotCurrentPose.get().getTranslation());
     return OriginToObject;
   }
 
+  /**
+   * Configures the Shuffleboard/SmartDashboard display for this subsystem.
+   * Adds the X and Y coordinates of the tracked object to the dashboard.
+   */
   @Override
   public void initSendable(SendableBuilder builder) {
       builder.addDoubleProperty("X", this::getX, null);
       builder.addDoubleProperty("Y", this::getY, null);
   }
 
+  
+  /**
+   * Returns the X coordinate of the object on the field.
+   * @return X position in field coordinates
+   */
   public double getX(){
     return this.OriginToObject.getX();
   }
 
+  /**
+   * Returns the Y coordinate of the object on the field.
+   * @return Y position in field coordinates
+   */
   public double getY(){
     return this.OriginToObject.getY();
   }
