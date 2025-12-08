ed71586dca6613fd5ff7da0695dabf8a9de8cccb
matan <diz.matan@gmail.com>
2025-12-08 19:45:37 +0200

    fixed your code or

--- src/main/java/frc/robot/UtilsVision/objectPos.java
+++ src/main/java/frc/robot/UtilsVision/objectPos.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 //import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -113,10 +114,10 @@ private NetworkTable Table;
   }
 
   public double getX(){
-    return this.OriginToObject.getX();
+    return this.originToObject.getX();
   }
 
   public double getY(){
-    return this.OriginToObject.getY();
+    return this.originToObject.getY();
   }
 }
